095e4af4266dbd934715fad954cb7ef421cb9b51
Maris Klaiva(Home) <maris.klaiva@gmail.com>
2023-06-11 12:48:26 +0300

    Start scene and transitions

---
+++ Assets/Scripts/ObjectScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScript : MonoBehaviour {
+
+	public GameObject howToPlayGroup;
+	public GameObject howToPanel;
+}
---
+++ Assets/Scripts/SceneStuff.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneStuff : MonoBehaviour
+{
+    public ObjectScript objectScript;
+    public string NextScene;
+
+    void Start()
+    {
+        
+    }
+
+    public void howToPlay(bool variable)
+    {
+        objectScript.howToPanel.SetActive(variable);
+        objectScript.howToPlayGroup.SetActive(variable);
+    }
+
+    public void toNextScene()
+    {
+        SceneManager.LoadScene(NextScene, LoadSceneMode.Single);
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
+
